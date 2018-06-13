34eaa2ebd78604fdfccc762c80939f731fc8feab
Harsha192 <harshagayan8@gmail.com>
2018-06-13 16:17:16 +0530

    firebase deployement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBmpsJcfVC1eFAtCFlwiLWvUAEsCqsGWvA",
+    authDomain: "employee-register-57b08.firebaseapp.com",
+    databaseURL: "https://employee-register-57b08.firebaseio.com",
+    projectId: "employee-register-57b08",
+    storageBucket: "employee-register-57b08.appspot.com",
+    messagingSenderId: "877213451771"
+  }
 };
